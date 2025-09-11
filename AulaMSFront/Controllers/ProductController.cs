193bf2c1985d559bc0cb033c02576fc1ec57e8da
Renato de Azevedo Lacerda <renatoads1@gmail.com>
2025-09-11 08:45:41 -0300

    product list

---
+++ AulaMSFront/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using AulaMSFront.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AulaMSFront.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetAllProductsAsync();
+            return View(products);
+        }
+    }
+}
